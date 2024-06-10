828a69f209553455b491fa5724ad084db76a8407
SidPic <sidpiclu@gmail.com>
2024-06-10 14:15:23 +0500

    operators priority fix

--- main.cpp
+++ main.cpp
@@ -9,10 +9,9 @@
 
 const std::string END_TOK {";"};
 const std::set<std::string> infix {
-    "+", "-", "*", "/",
-    "#", "%", "^", ".",
-    "<", ">", "<=", ">=",
-    "-r", "/r", "<r", ">r", "<=r", ">=r", "%r", "^r", "+r", "*r"
+    "+", "-", "*", "/",     "+r", "-r", "*r", "/r",
+    "#", "%", "^", ".",     "#r", "%r", "^r", ".r",
+    "<", ">", "<=", ">=",   "<r", ">r", "<=r", ">=r"
 };
 
 const std::map<std::string, unsigned char> priority {
@@ -22,9 +21,11 @@ const std::map<std::string, unsigned char> priority {
     { "/", 2 }, { "/r", 2 },
     { "%", 1 }, { "%r", 1 },
     { "^", 1 }, { "^r", 1 },
+    { "#", 0 }, { "#r", 0 },
+    { ".", 0 }, { ".r", 0 },
     { "<", 4 }, { "<r", 4 },
-    { "<=", 4 }, { "<=r", 4 },
     { ">", 4 }, { ">r", 4 },
+    { "<=", 4 }, { "<=r", 4 },
     { ">=", 4 }, { ">=r", 4 }
 };
 
@@ -59,7 +60,7 @@ again:
         }
     }
     else if (isalnum(ch)) {
-        while (isalnum(ch) || ch == '#' || ch == '.') {
+        while (isalnum(ch)) {
             token.push_back(ch);
             ch = fgetc(file);
         }
@@ -99,8 +100,12 @@ int main() {
 
         for (int comma = 0; comma < tok.size(); ++comma) {
             couples.emplace_back();
+            bool has_brackets = false;
 
-            while (comma < tok.size() && tok[comma] != ",") couples.back().push_back(tok[comma++]);
+            while (comma < tok.size() && tok[comma] != ",") {
+                if (tok[comma] == "(") has_brackets = true;
+                couples.back().push_back(tok[comma++]);
+            }
 
             #undef tok
             size_t begin = couples.size()-1;
@@ -109,53 +114,70 @@ int main() {
             int token = 0;
             std::function<void()> parse_brackets;
             parse_brackets = [&](){
-                couples.emplace_back();
+                bool has_infix = false;
+
+                if (has_brackets) couples.emplace_back();
+
                 size_t couple = couples.size()-1;
 
                 for (; token < tok.size(); ++token) {
-                    if (tok[token] == "(") {
+                    if (has_brackets && tok[token] == "(") {
                         ++token;
                         parse_brackets();
                     }
-                    else if (tok[token] == ")") {
+                    else if (has_brackets && tok[token] == ")") {
                         break;
                     }
                     else {
-                        couples[couple].push_back(
-                        (token+1 < tok.size() && tok[token+1] == "(" && isinfix(tok[token])) ?
-                            tok[token] + "r" : tok[token]
-                        );
-                    }
-                } // сделать аналогично для более приоритетных операторов
-
-                #undef tok
-                #define tok couples[couple]
-                std::vector<std::string> ptoks;
-                std::vector<bool> is_moved ( tok.size(), false );
-
-                for (int p = 0; p <= 7; ++p) {
-                    for (int n = 0; n < tok.size(); ++n) {
-                        if (isinfix(tok[n]) && priority.at(tok[n]) == p)
-                        {
-                            if (p > 0 && !isinfix(tok[n-1]) && !is_moved[n-1]) {
-                                ptoks.push_back(tok[n-1]);
-                                is_moved[n-1] = true;
+                        //~ std::cout << couple << " | " << tok[token] << std::endl; /// DEBUG
+                        if (isinfix(tok[token])) {
+                            has_infix = true;
+                            if (has_brackets) {
+                                if (token+2 < tok.size() && isinfix(tok[token+2])) {
+                                    if (priority.at(tok[token+2]) < priority.at(tok[token])) {
+                                        couples[couple].push_back(tok[token] + "r");
+                                        continue;
+                                    }
+                                }
+                                if (token+1 < tok.size() && tok[token+1] == "(") {
+                                    couples[couple].push_back(tok[token] + "r");
+                                    continue;
+                                }
+                                couples[couple].push_back(tok[token]);
                             }
-                            if (p < 7 && !isinfix(tok[n+1]) && !is_moved[n+1]) {
-                                ptoks.push_back(tok[n+1]);
-                                is_moved[n+1] = true;
-                            }
-                            ptoks.push_back(tok[n]);
-                        }
+                        } else if (has_brackets) couples[couple].push_back(tok[token]);
                     }
                 }
 
-                if (!ptoks.empty()) tok = ptoks;
+                if (has_infix) {
+                    #undef tok
+                    #define tok couples[couple]
+                    std::vector<std::string> ptoks;
+                    std::vector<bool> is_moved ( tok.size(), false );
 
-                #undef tok
+                    for (int p = 0; p <= 7; ++p) {
+                        for (int n = 0; n < tok.size(); ++n) {
+                            if (isinfix(tok[n]) && priority.at(tok[n]) == p)
+                            {
+                                if (n > 0 && !isinfix(tok[n-1]) && !is_moved[n-1]) {
+                                    ptoks.push_back(tok[n-1]);
+                                    is_moved[n-1] = true;
+                                }
+                                if (n+1 < tok.size() && !isinfix(tok[n+1]) && !is_moved[n+1]) {
+                                    ptoks.push_back(tok[n+1]);
+                                    is_moved[n+1] = true;
+                                }
+                                ptoks.push_back(tok[n]);
+                                is_moved[n] = true;
+                            }
+                        }
+                    }
+                    if (!ptoks.empty()) tok = ptoks;
+                    #undef tok
+                }
             };
             parse_brackets();
-            couples.erase(couples.begin()+begin);
+            if (has_brackets) couples.erase(couples.begin() + begin);
         }
         couples.erase(couples.begin() + begin);
 
@@ -166,6 +188,14 @@ int main() {
     std::vector<std::string> script;
     auto cur = script.begin();
 
+    ///DEBUG
+    //~ for (auto& c : couples) {
+        //~ for (auto& s : c) {
+            //~ std::cout << s << " ";
+        //~ }
+        //~ std::cout << std::endl;
+    //~ }
+
     for (int i = 0; i < couples.size(); ++i) {
         size_t begin = i;
         while (couples[i][0] != END_TOK) ++i;
@@ -173,13 +203,6 @@ int main() {
             #define c (*c)
 
             if (c.empty()) continue;
-            if (c[0] == END_TOK) {
-
-                //~ std::cout << END_TOK << std::endl;///DEBUG
-
-                script.push_back(END_TOK);
-                continue;
-            }
 
             bool has_infix = false;
             for (auto& tok : c) if (isinfix(tok)) {
@@ -200,8 +223,10 @@ int main() {
             //~ std::cout << std::endl; ///DEBUG
             #undef c
         }
+        script.push_back(END_TOK);
     }
 
+    ///DEBUG
     for (auto& cmd : script) {
         std::cout << cmd << std::endl;
     }
--- script.ll
+++ script.ll
@@ -1,3 +1,3 @@
-var x, 10, object.func asd arr#12 32, 2 - (53 - (32 + 10 / o.a * (sum 1 2)) - 21) * 100
+var x, 10, object.func.a, asd, arr#(12 - 1), 11, 2 - (53 - (32 - 10 / o.a * (sum 1 2)) - 21) * 100
 set y 10
 say "hello world"
